dd48b800f197915665f75abbcbfade39172dbeab
NeoArmageddon <mail@anzp.de>
2018-04-22 11:09:16 +0200

    @ Now using pythons copy instead of copyfile for artifacts

--- compile.py
+++ compile.py
@@ -63,7 +63,7 @@ for mission in missions:
                     f.flush()
                     f.close()
     subprocess.call(["cpbo.exe", "-p", missiondir])
-    shutil.copyfile(missiondir + ".pbo", './Artifacts/Missions/') #Copy build artifact
+    shutil.copy(missiondir + ".pbo", './Artifacts/Missions/') #Copy build artifact
 addons = data['Addons'];
 t = []
 for m in missions:
